bc8b9689f4f4cd1ca0caa1f8bfbcd7e452ea5bcd
323103705 <rotemr2001@gmail.com>
2025-11-06 13:22:38 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,14 +1,5 @@
-import random
 def approximate_pi(n_terms):
-    radius = 0.5
-    count_in_circle = 0
-    count_in_squre = 0
     for i in range(n_terms):
-      x = random.random()
-      y = random.random()
-      distance_to_center = ((x - radius) ** 2 + (y - radius) ** 2) ** 0.5
-      if distance_to_center < radius:
-        count_in_circle += 1
-      count_in_squre += 1
-    pai = (count_in_circle / count_in_squre) * 4
+      distance_to_center = (-1 ** i + 2 * i + 1)
+    pai = distance_to_center * 4
     return pai
