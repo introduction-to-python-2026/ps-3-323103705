24496a8e49f72f11cc236b5cdbe53e4328f2a508
323103705 <rotemr2001@gmail.com>
2025-11-11 23:39:02 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,6 +1,5 @@
 def approximate_pi(n_terms):
-    distance_to_center = 0
+    pi = 0
     for i in range(n_terms):
-      distance_to_center += (((-1) ** i) / (2 * i + 1))
-    pai = distance_to_center * 4
-    return pai
+      pi += 4 * (((-1) ** i) / (2 * i + 1))
+    return pi
